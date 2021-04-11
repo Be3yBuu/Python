7f6bf55d82b9513027eb560274fb3070f17624f1
Йоукай <lliaxudbi@mail.ru>
2021-04-11 21:49:22 +1000

    lesson3

--- 3rd lesson/Chashchin_Anton_3_5.py
+++ 3rd lesson/Chashchin_Anton_3_5.py
@@ -4,7 +4,7 @@ import random
 def get_jokes(number, flag=0):
     """
     Function of jokes
-    :param number: number of list objects we need (maximum 5)
+    :param number: number of list objects we need (if flag - maximum 5)
     :param flag: if exists - no repeats
     :return: list of concat strings of 3 random words from pool
     """
@@ -13,9 +13,12 @@ def get_jokes(number, flag=0):
     adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
     adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
     if flag:
-        for i in range(0, number):
-            final_list.append(nouns.pop(random.randint(0, 4-i)) + ' ' + adverbs.pop(random.randint(0, 4-i)) + ' ' +
-                              adjectives.pop(random.randint(0, 4-i)))
+        if number > 5:
+            final_list.append('Комбинаций может быть не больше 5')
+        else:
+            for i in range(0, number):
+                final_list.append(nouns.pop(random.randint(0, 4-i)) + ' ' + adverbs.pop(random.randint(0, 4-i)) + ' ' +
+                                  adjectives.pop(random.randint(0, 4-i)))
     else:
         for i in range(0, number):
             final_list.append(nouns[random.randint(0, 4)]+' '+adverbs[random.randint(0, 4)]+' ' +
