398420b8934c06411debcc3c204d82851419015b
Йоукай <lliaxudbi@mail.ru>
2021-05-02 18:30:23 +1000

    9th lesson

--- 9th lesson/Chashchin_Anton_9_2.py
+++ 9th lesson/Chashchin_Anton_9_2.py
@@ -3,15 +3,12 @@
 # которая физического смысла не имеет
 # использую формулу: длина * ширина * высота * плотность
 class Road:
-    def __init__(self, length, width, height=0.01):  # 1 см толщиной
+    def __init__(self, length, width, height=1):
         self._length = length
         self._width = width
         self._height = height
-        self._mass = self._length * self._width * self._height * 1500
-
-    def mass(self):
-        return self._mass
+        self._mass = self._length * self._width * self._height * 0.00233  # мелкозернистый асфальт 2330кг/м^3
 
 
-road = Road(5000, 20)  # расчет в метрах
-print(road.mass, 'кг')
+road = Road(5000, 20, 5)  # (метр, метр, сантиметр))
+print(road._mass, 'тонн')
