e2c2375feed9ea682689caad1cf0e398bed33140
Йоукай <lliaxudbi@mail.ru>
2021-05-04 22:23:28 +1000

    9.1

--- 9th lesson/Chashchin_Anton_9_1.py
+++ 9th lesson/Chashchin_Anton_9_1.py
@@ -1,37 +1,33 @@
-from tkinter import *
-from time import sleep
-from itertools import cycle
+from tkinter import Tk, Canvas
 
-run = {'red': 7, 'yellow': 2, 'green': 5}
+run = {'red': 7000, 'yellow': 2000, 'green': 5000}
+light_number = 0
 
 
-class TrafficLights:
-    def __init__(self, color):
-        _color = color
-        window = Tk()
-        window.title("Traffic Light")
+class TrafficLights(Tk):
+    def __init__(self):
+        super().__init__()
+        _color = list(run)
 
-        self.canvas = Canvas(window, width=340, height=120, bg="white")
-        self.canvas.pack()
+        self.canvas = Canvas(self, width=340, height=120, bg="white")
+        self.canvas.grid()
 
         self.oval_red = self.canvas.create_oval(10, 10, 110, 110, fill="white", outline=_color[0])
         self.oval_yellow = self.canvas.create_oval(120, 10, 220, 110, fill="white", outline=_color[1])
         self.oval_green = self.canvas.create_oval(230, 10, 330, 110, fill="white", outline=_color[2])
-        self.running(_color)
+        self.running()
 
-    def running(self, col):
-        for item in cycle(col):
-            self.canvas.itemconfigure(self.oval_red, fill='white')
-            self.canvas.itemconfigure(self.oval_yellow, fill='white')
-            self.canvas.itemconfigure(self.oval_green, fill='white')
-            if item == 'red':
-                self.canvas.itemconfigure(self.oval_red, fill='red')
-            if item == 'yellow':
-                self.canvas.itemconfigure(self.oval_yellow, fill='yellow')
-            if item == 'green':
-                self.canvas.itemconfigure(self.oval_green, fill='green')
-            self.canvas.update()
-            sleep(run[item])
+    def running(self):
+        global light_number
+        self.canvas.itemconfigure(self.oval_red, fill='white')
+        self.canvas.itemconfigure(self.oval_yellow, fill='white')
+        self.canvas.itemconfigure(self.oval_green, fill='white')
+        exec(f"self.canvas.itemconfigure(self.oval_{list(run)[light_number]}, fill='{list(run)[light_number]}')")
+        light_number += 1
+        if light_number == 3:
+            light_number = 0
+        self.after(run[list(run)[light_number - 1]], self.running)
 
 
-light = TrafficLights(list(run))
+root = TrafficLights()
+root.mainloop()
