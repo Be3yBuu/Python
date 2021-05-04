dab0de122ee9019422e3f3fd86ed86d89d1edf8f
Йоукай <lliaxudbi@mail.ru>
2021-05-04 23:03:16 +1000

    9.1

--- 9th lesson/Chashchin_Anton_9_1.py
+++ 9th lesson/Chashchin_Anton_9_1.py
@@ -5,16 +5,16 @@ light_number = 0
 
 
 class TrafficLights(Tk):
+    _color = list(run)
+
     def __init__(self):
         super().__init__()
-        _color = list(run)
-
         self.canvas = Canvas(self, width=340, height=120, bg="white")
         self.canvas.grid()
 
-        self.oval_red = self.canvas.create_oval(10, 10, 110, 110, fill="white", outline=_color[0])
-        self.oval_yellow = self.canvas.create_oval(120, 10, 220, 110, fill="white", outline=_color[1])
-        self.oval_green = self.canvas.create_oval(230, 10, 330, 110, fill="white", outline=_color[2])
+        self.oval_red = self.canvas.create_oval(10, 10, 110, 110, fill="white", outline=self._color[0])
+        self.oval_yellow = self.canvas.create_oval(120, 10, 220, 110, fill="white", outline=self._color[1])
+        self.oval_green = self.canvas.create_oval(230, 10, 330, 110, fill="white", outline=self._color[2])
         self.running()
 
     def running(self):
@@ -22,11 +22,11 @@ class TrafficLights(Tk):
         self.canvas.itemconfigure(self.oval_red, fill='white')
         self.canvas.itemconfigure(self.oval_yellow, fill='white')
         self.canvas.itemconfigure(self.oval_green, fill='white')
-        exec(f"self.canvas.itemconfigure(self.oval_{list(run)[light_number]}, fill='{list(run)[light_number]}')")
+        exec(f"self.canvas.itemconfigure(self.oval_{self._color[light_number]}, fill='{self._color[light_number]}')")
         light_number += 1
         if light_number == 3:
             light_number = 0
-        self.after(run[list(run)[light_number - 1]], self.running)
+        self.after(run[self._color[light_number - 1]], self.running)
 
 
 root = TrafficLights()
